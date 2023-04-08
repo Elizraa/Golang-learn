b3effaf5efa407480a8612e496f29efff0b8c658
Elizra <azrielwah@gmail.com>
2023-04-09 04:39:01 +0700

    fix go-sql

--- go-sql/pkg/utils/utils.go
+++ go-sql/pkg/utils/utils.go
@@ -1,9 +1,15 @@
 package utils
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
 )
 
-func ParseBody(r *http.Request, X interface{}) {
-
+func ParseBody(r *http.Request, x interface{}) {
+	if body, err := io.ReadAll(r.Body); err != nil {
+		if err := json.Unmarshal([]byte(body), x); err != nil {
+			return
+		}
+	}
 }
